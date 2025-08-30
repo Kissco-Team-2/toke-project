6e6a5a336fc0fb2ff197aadf768c7e73d7ebd558
agreenroom <tjwlals7805@gmail.com>
2025-08-30 19:09:06 +0900

    first commit

---
+++ src/main/java/com/toke/toke_project/TokeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.toke.toke_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toke/toke_project/TokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toke.toke_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
